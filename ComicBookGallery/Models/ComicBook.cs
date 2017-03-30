dc37f98f77ede92aeb58a1c61dcbd07c03cb08a3
Matthew Pingel <m_ping@live.com>
2017-03-29 21:44:06 -0400

    updated comicbook detail view to be stronly typed

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -14,7 +14,7 @@ namespace ComicBookGallery.Models
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
-        public string DisplayTest
+        public string DisplayText
         {
             get
             {
